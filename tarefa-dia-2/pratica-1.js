0c9d907c2629b9bb25b76574bc9e58d67132c274
Anderson Roberto Da Silva <andersongmu@gmail.com>
2022-08-24 19:36:26 -0300

    adicionando valores de variável dentro de uma string

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,11 +1,14 @@
-let nome = ("Anderson Roberto Da Silva")
-let email = ("andersongmu@gmail.com")
-let profissao = ("Lubrificador Industrial")
-let idade = 22
-let viagemDosSonhos = ("Japão")
+let nome = ("Anderson Roberto Da Silva");
+let email = ("andersongmu@gmail.com");
+let profissao = ("Lubrificador Industrial");
+let idade = 22;
+let viagemDosSonhos = ("Japão");
 
-console.log("Nome: ", nome)
-console.log("E-mail: ", email)
-console.log("Profissão: ", profissao)
-console.log("Idade: ", idade)
-console.log("Para onde sonho em viajar? ", viagemDosSonhos)
+console.log("Nome: ", nome);
+console.log("E-mail: ", email);
+console.log("Profissão: ", profissao);
+console.log("Idade: ", idade);
+console.log("Para onde sonho em viajar? ", viagemDosSonhos);
+
+var mensagemTurismo = `Olá, ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${viagemDosSonhos}. Confirma pra gente alguns dados? A sua idade é ${idade}, você é ${profissao} e o seu email é ${email}? Ficamos no aguardo. Muito obrigado e boa viagem! :D`;
+console.log(mensagemTurismo);
