4ecfcdd6bbea81b0da55e06ffdc9bfc7c89f18e1
Anderson Roberto Da Silva <andersongmu@gmail.com>
2022-08-24 19:14:10 -0300

    entendendo variaveis e valores

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -3,3 +3,9 @@ let email = ("andersongmu@gmail.com")
 let profissao = ("Lubrificador Industrial")
 let idade = 22
 let viagemDosSonhos = ("Japão")
+
+console.log("Nome: ", nome)
+console.log("E-mail: ", email)
+console.log("Profissão: ", profissao)
+console.log("Idade: ", idade)
+console.log("Para onde sonho em viajar? ", viagemDosSonhos)
